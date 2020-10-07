f43a968b6704f86f5c97e62c4909ad422decb960
Yogendra Tamang <yogen48@hotmail.com>
2020-10-07 21:36:13 +0200

    ci update

--- 1/to_dockerhub.sh
+++ 1/to_dockerhub.sh
@@ -1,7 +1,10 @@
 #! /bin/sh
+cd "$(dirname "$0")"
 
 docker build -t simple-flask-app .
 
-docker tag simple-flask-app yogen48/simple-flask-app:countries
+docker tag simple-flask-app yogen48/simple-flask-app:latest
 
-docker push yogen48/simple-flask-app:countries
+docker push yogen48/simple-flask-app:latest
+
+echo "Latest Image pushed"
