ad1fa2c666bd2aab2cdc0534217ed1b74e7768e1
Yogendra Tamang <yogen48@hotmail.com>
2020-10-07 22:05:35 +0200

    update default welcome message

--- 1/src/app.py
+++ 1/src/app.py
@@ -12,7 +12,7 @@ country_list = [
 @app.route('/')
 def index():
     response_dict = {
-        "message": "Welcome to FlaskApp"
+        "message": "Welcome to FlaskApp with CI"
     }
     return jsonify(response_dict)
 
