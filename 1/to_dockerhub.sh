54b1478058dbc942b004bc4ac9f433a73422447d
Yogendra Tamang <yogen48@hotmail.com>
2020-10-07 21:54:23 +0200

    dev endpoint add

--- 1/to_dockerhub.sh
+++ 1/to_dockerhub.sh
@@ -3,8 +3,8 @@ cd "$(dirname "$0")"
 
 docker build -t simple-flask-app .
 
-docker tag simple-flask-app yogen48/simple-flask-app:latest
+docker tag simple-flask-app yogen48/simple-flask-app:dev
 
-docker push yogen48/simple-flask-app:latest
+docker push yogen48/simple-flask-app:dev
 
-echo "Latest Image pushed"
+echo "dev Image pushed"
